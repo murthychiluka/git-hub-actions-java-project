1b627ab5f9d911da1eda23ee9e30ee824455521f
murthychiluka <murthychiluka@gmail.com>
2026-08-11 14:28:54 +0530

    added content

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,5 @@
+server.port=8080
+spring.datasource.url=jdbc:mysql://${DB_HOST}:3306/${DB_NAME}
+spring.datasource.username=${DB_USERNAME}
+spring.datasource.password=${DB_PASSWORD}
+spring.jpa.hibernate.ddl-auto=update
