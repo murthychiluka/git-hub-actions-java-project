b9c2ed2b156c83c34bb3b09e653fb7dc39b2922e
murthychiluka <murthychiluka@gmail.com>
2026-08-11 15:12:47 +0530

    hello controller

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,13 @@
-server.port=8080
-spring.datasource.url=jdbc:mysql://${DB_HOST}:3306/${DB_NAME}
-spring.datasource.username=${DB_USERNAME}
-spring.datasource.password=${DB_PASSWORD}
-spring.jpa.hibernate.ddl-auto=update
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Java app, connected to RDS!";
+    }
+}
